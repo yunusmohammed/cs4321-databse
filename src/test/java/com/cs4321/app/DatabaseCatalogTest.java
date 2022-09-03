4dac8257e0ad2ca9344e9a673ae90cd4bf6eb9bd
Yohanes Kidane <ysk27@cornell.edu>
2022-09-03 18:21:30 -0400

    modified database catalog and moved input folder

--- src/test/java/com/cs4321/app/DatabaseCatalogTest.java
+++ src/test/java/com/cs4321/app/DatabaseCatalogTest.java
@@ -18,7 +18,7 @@ class DatabaseCatalogTest {
 	
 	DatabaseCatalog dbc = DatabaseCatalog.getInstance();
 	static String sep = File.separator;
-	static String inputdir = System.getProperty("user.dir") + File.separator + "input";
+	static String inputdir = System.getProperty("user.dir") + sep + "src" + sep + "test" + sep + "java" + sep + "input";
 	
 	@BeforeAll
 	static void setup() {
@@ -27,15 +27,21 @@ class DatabaseCatalogTest {
 
 	@Test
 	void tablePathTest() {
+		// sailors path
 		assertEquals(inputdir + sep + "db" + sep + "data" + sep + "Sailors.txt", dbc.tablePath("Sailors"));
+		// boats path
 		assertEquals(inputdir + sep + "db" + sep + "data" + sep + "Boats.txt", dbc.tablePath("Boats"));
+		// reserves path
 		assertEquals(inputdir + sep + "db" + sep + "data" + sep + "Reserves.txt", dbc.tablePath("Reserves"));
 	}
 	
 	@Test
 	void tableSchemaTest() {
+		// sailors schema
 		assertArrayEquals(new String[]{"Sailors", "A", "B", "C"}, dbc.tableSchema("Sailors"));
+		// boats schema
 		assertArrayEquals(new String[]{"Boats", "D", "E", "F"}, dbc.tableSchema("Boats"));
+		// reserves schema
 		assertArrayEquals(new String[]{"Reserves", "G", "H"}, dbc.tableSchema("Reserves"));
 	}
 

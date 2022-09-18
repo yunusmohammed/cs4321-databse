3f33ecc30e9c5f1c17538fc504b490d055987994
Yunus Maltiti Mohammed <ymm26@cornell.edu>
2022-09-18 15:17:01 -0400

    Comment test utils

--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -15,6 +15,15 @@ import net.sf.jsqlparser.statement.select.Select;
  *         Contains test utility functions
  */
 public class Utils {
+
+  /**
+   * Gets the where expression from the query specified by tableNames and s
+   * 
+   * @param tableNames comma separated string of the names of the tables to query
+   * @param s          the where condition of the query
+   * @return the where expression of the query specified by tableNames and s
+   * @throws ParseException
+   */
   public static Expression getExpression(String tableNames, String s) throws ParseException {
     String query = "SELECT * FROM " + tableNames + " WHERE " + s + ";";
     StringReader reader = new StringReader(query);

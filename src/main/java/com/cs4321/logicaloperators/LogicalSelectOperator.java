5da6e2b610a4d49024e11355f38f73c29ba9c6ae
Yunus Maltiti Mohammed <ymm26@cornell.edu>
2022-09-29 15:47:18 -0400

    Implement LogicalSelectOperator

---
+++ src/main/java/com/cs4321/logicaloperators/LogicalSelectOperator.java
@@ -0,0 +1,43 @@
+package com.cs4321.logicaloperators;
+
+import net.sf.jsqlparser.expression.Expression;
+
+/**
+ * A Logical Select Operator
+ * 
+ * @author Yunus (ymm26@cornell.edu)
+ */
+public class LogicalSelectOperator extends LogicalOperator {
+  private final Expression selectCondition;
+  private final LogicalScanOperator child;
+
+  /**
+   * Creates a logical select operator
+   * 
+   * @param selectCondition The selection condition of the operator
+   * @param child           The child logical scan operator of the logical select
+   *                        operator
+   */
+  public LogicalSelectOperator(Expression selectCondition, LogicalScanOperator child) {
+    this.selectCondition = selectCondition;
+    this.child = child;
+  }
+
+  /**
+   * Get the child logical scan operator of the logical select
+   * 
+   * @return The child logical scan operator of the logical select
+   */
+  public LogicalScanOperator getChildOperator() {
+    return this.child;
+  }
+
+  /**
+   * Get the selection condition of the operator
+   * 
+   * @return The selection condition of the operator
+   */
+  public Expression getSelectCondition() {
+    return this.selectCondition;
+  }
+}

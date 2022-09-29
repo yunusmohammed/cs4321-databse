492f388be2c1f64cd79327db09091709b7974579
Yunus Maltiti Mohammed <ymm26@cornell.edu>
2022-09-29 19:14:45 -0400

    Implement and unit test LogicalDuplicateEliminationOperator

--- src/main/java/com/cs4321/logicaloperators/LogicalDuplicateEliminationOperator.java
+++ src/main/java/com/cs4321/logicaloperators/LogicalDuplicateEliminationOperator.java
@@ -1,5 +1,30 @@
 package com.cs4321.logicaloperators;
 
+/**
+ * A Logical Duplicate Elimination Operator
+ * 
+ * @author Yunus (ymm26@cornell.edu)
+ */
 public class LogicalDuplicateEliminationOperator extends LogicalJoinOperator {
 
+  private LogicalOperator child;
+
+  /**
+   * Construct a logical duplicate elimination operator
+   * 
+   * @param child the logical child operator of this logical duplicate elimination
+   *              operator
+   */
+  public LogicalDuplicateEliminationOperator(LogicalOperator child) {
+    this.child = child;
+  }
+
+  /**
+   * Get the logical child operator
+   * 
+   * @return The logical child operator
+   */
+  public LogicalOperator getChild() {
+    return this.child;
+  }
 }

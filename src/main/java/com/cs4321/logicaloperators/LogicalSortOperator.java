3e53bfbd84a672135f5b82f21ef2afc2e232a9e9
Yunus Maltiti Mohammed <ymm26@cornell.edu>
2022-09-29 18:49:00 -0400

    Implement LogicalSortOperator

--- src/main/java/com/cs4321/logicaloperators/LogicalSortOperator.java
+++ src/main/java/com/cs4321/logicaloperators/LogicalSortOperator.java
@@ -1,5 +1,48 @@
 package com.cs4321.logicaloperators;
 
+import java.util.List;
+
+import net.sf.jsqlparser.statement.select.OrderByElement;
+
+/**
+ * A Logical Sort Operator
+ * 
+ * @author Yunus (ymm26@cornell.edu)
+ */
 public class LogicalSortOperator extends LogicalOperator {
 
+  private final LogicalOperator child;
+  private final List<OrderByElement> orderByElementList;
+
+  /**
+   * Constructor of the logical sort operator
+   * 
+   * @param child           The logical child operator
+   * @param orderByElements the list of elements for which our order by clause
+   *                        will sort
+   */
+  public LogicalSortOperator(LogicalOperator child, List<OrderByElement> orderByElements) {
+    this.child = child;
+    this.orderByElementList = orderByElements;
+  }
+
+  /**
+   * Get the logical child operator
+   * 
+   * @return The logical child operator
+   */
+  public LogicalOperator getChild() {
+    return this.child;
+  }
+
+  /**
+   * Get the list of elements for which our order by clause
+   * will sort
+   * 
+   * @return The list of elements for which our order by clause
+   *         will sort
+   */
+  public List<OrderByElement> getOrderByElements() {
+    return this.orderByElementList;
+  }
 }

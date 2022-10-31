ab633ffdf30e946ebbe4c6fb75bb555927480728
Jessica Tweneboah <jnt42@cornell.edu>
2022-10-31 18:31:24 -0400

    Javadoc comments

--- src/main/java/com/cs4321/indexes/BPlusTree.java
+++ src/main/java/com/cs4321/indexes/BPlusTree.java
@@ -4,12 +4,17 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 /**
- * This is the B+ Tree
+ * This is the B+ Tree for building indexes in the database. It is implemented
+ * using the bulk loading algorithm
  */
 public class BPlusTree {
     BPlusTreeSerializer bPlusTreeSerializer;
     int order;
 
+    /**
+     * Initialises a new B+ Tree object
+     * @param filename The file in which the B+ tree is going to be serialized to
+     */
     public BPlusTree(String filename) {
         try {
             this.bPlusTreeSerializer = new BPlusTreeSerializer(filename);
@@ -25,10 +30,20 @@ public class BPlusTree {
 
     void buildLeafLayer() {
         //TODO Yohanes
+        try {
+            bPlusTreeSerializer.writeLeafNodeToPage(null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     void buildIndexLayer() {
         //TODO Yohanes
+        try {
+            bPlusTreeSerializer.writeIndexNodeToPage(null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
--- src/main/java/com/cs4321/indexes/LeafNode.java
+++ src/main/java/com/cs4321/indexes/LeafNode.java
@@ -3,22 +3,43 @@ package com.cs4321.indexes;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * LeafNode class representing leaf nodes in the B+ Tree
+ */
 public class LeafNode extends Node {
     List<DataEntry> dataEntries;
     int numberOfDataEntires;
 
+    /**
+     * Sets the node flag to 0 representing a leaf node
+     */
     LeafNode() {
         super(0);
     }
 
+    /**
+     * Returns the number of data entries in the node
+     *
+     * @return the number of data entries in the node
+     */
     public int getNumberOfDataEntires() {
         return numberOfDataEntires;
     }
 
+    /**
+     * Returns data entries in the node
+     *
+     * @return data entries in the node
+     */
     public List<DataEntry> getDataEntries() {
         return dataEntries;
     }
 
+    /**
+     * Adds dataEntry to the list of dataEntries
+     *
+     * @param dataEntry of the format < k, [(p1, t1),(p2, t2), · · ·(pk, tk)] >
+     */
     public void addDataEntry(DataEntry dataEntry) {
         dataEntries.add(dataEntry);
         numberOfDataEntires++;
@@ -35,19 +56,40 @@ public class LeafNode extends Node {
         int key;
         int numberOfRids;
 
+        /**
+         * Initialises a new DataEntry object
+         *
+         * @param key is the (integer) search key for the index
+         */
         DataEntry(int key) {
             this.key = key;
         }
 
+        /**
+         * Initialises a new DataEntry object
+         *
+         * @param key  is the (integer) search key for the index
+         * @param rids is a list of record ids (rids)
+         */
         DataEntry(int key, List<Rid> rids) {
             this(key);
             this.rids = rids;
         }
 
+        /**
+         * Returns the number of record ids in this DataEntry
+         *
+         * @return the number of record ids in this DataEntry
+         */
         public int getNumberOfRids() {
             return numberOfRids;
         }
 
+        /**
+         * Adds rid to the list of record ids
+         *
+         * @param rid is a record id
+         */
         public void addRid(Rid rid) {
             this.rids.add(rid);
             numberOfRids++;
@@ -64,6 +106,12 @@ public class LeafNode extends Node {
         int pageId;
         int tupleId;
 
+        /**
+         * Initialises a new Rid object
+         *
+         * @param pageId  is the number of the page the tuple is on
+         * @param tupleId is the number of the tuple on the page numbered pageid
+         */
         Rid(int pageId, int tupleId) {
             this.pageId = pageId;
             this.tupleId = tupleId;

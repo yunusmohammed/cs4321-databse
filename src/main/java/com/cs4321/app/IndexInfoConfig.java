746dda2b5adad965982ed42a232ed5add858ca2d
Jessica Tweneboah <jnt42@cornell.edu>
2022-11-02 20:36:34 -0400

    Changing IndexInfoList to IndexInfoMap

--- src/main/java/com/cs4321/app/IndexInfoConfig.java
+++ src/main/java/com/cs4321/app/IndexInfoConfig.java
@@ -3,14 +3,15 @@ package com.cs4321.app;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Jessica Tweneboah
  */
 public class IndexInfoConfig {
-    List<IndexInfo> indexInfoList;
+    Map<String, IndexInfo> indexInfoMap;
 
     /**
      * Initialises an IndexInfoConfig Object
@@ -18,29 +19,29 @@ public class IndexInfoConfig {
      * @param indexInfofilePath The path to the Index Info Config File
      */
     public IndexInfoConfig(String indexInfofilePath) {
-        indexInfoList = new ArrayList<>();
+        indexInfoMap = new HashMap<>();
         try {
             List<String> configLines = Files.readAllLines(Paths.get(indexInfofilePath));
             for (String configLine : configLines) {
                 String[] indexConfig = configLine.split(" ");
-                IndexInfo indexInfo = new IndexInfo(
-                        indexConfig[0],
-                        indexConfig[1],
-                        Integer.parseInt(indexConfig[2]) == 1,
-                        Integer.parseInt(indexConfig[3]));
-                indexInfoList.add(indexInfo);
+                IndexInfo indexInfo = new IndexInfo(indexConfig[0], indexConfig[1], Integer.parseInt(indexConfig[2]) == 1, Integer.parseInt(indexConfig[3]));
+                indexInfoMap.put(String.format("%s.%s", indexInfo.getRelationName(), indexInfo.getAttributeName()), indexInfo);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
+
     /**
-     * Returns a list of IndexInfo Objects that store information that specifies how indexes are built
+     * Returns a map that maps full column name to IndexInfo Objects that store information that
+     * specifies how indexes are built
      *
-     * @return A list of IndexInfo Objects that store information that specifies how indexes are built
+     * @return A map that maps full column name to IndexInfo Objects that store information that
+     * specifies how indexes are built
+     * Eg: Boats.E -> IndexInfo< RelationName: Boats, AttributeName: E, Clustered: 0, Order: 10 >
      */
-    public List<IndexInfo> getIndexInfoList() {
-        return indexInfoList;
+    public Map<String, IndexInfo> getIndexInfoMap() {
+        return indexInfoMap;
     }
 }

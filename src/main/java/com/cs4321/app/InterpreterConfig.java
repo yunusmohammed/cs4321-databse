07f3a8a2c08daf9ddf8f0f4ae0899de030c513cc
Jessica Tweneboah <jnt42@cornell.edu>
2022-11-29 22:47:06 -0500

    Refactoring Interpreter Config

--- src/main/java/com/cs4321/app/InterpreterConfig.java
+++ src/main/java/com/cs4321/app/InterpreterConfig.java
@@ -11,8 +11,8 @@ public class InterpreterConfig {
     private String inputdir;
     private String outputdir;
     private String tempdir;
-    private boolean evaluateQueries;
-    private boolean buildIndexes;
+//    private boolean evaluateQueries;
+//    private boolean buildIndexes;
     private boolean humanReadable = false;
 
     /**
@@ -23,16 +23,16 @@ public class InterpreterConfig {
     public InterpreterConfig(String filePath) {
         try {
             List<String> configLines = Files.readAllLines(Paths.get(filePath));
-            if (configLines.size() < 5) {
+            if (configLines.size() < 3) {
                 Logger.getInstance().log("Invalid format for Interpreter config file");
                 throw new Exception("Invalid format for Interpreter config file");
             }
             this.inputdir = configLines.get(0);
             this.outputdir = configLines.get(1);
             this.tempdir = configLines.get(2);
-            this.buildIndexes = Integer.parseInt(configLines.get(3)) == 1;
-            this.evaluateQueries = Integer.parseInt(configLines.get(4)) == 1;
-            if (configLines.size() == 6) {
+//            this.buildIndexes = Integer.parseInt(configLines.get(3)) == 1;
+//            this.evaluateQueries = Integer.parseInt(configLines.get(4)) == 1;
+            if (configLines.size() == 4) {
                 this.humanReadable = Integer.parseInt(configLines.get(5)) == 1;
             }
         } catch (Exception e) {
@@ -109,18 +109,18 @@ public class InterpreterConfig {
      * @return a flag to indicate whether the interpreter should actually evaluate the SQL queries
      * (false = no, true = yes)
      */
-    public boolean shouldEvaluateQueries() {
-        return evaluateQueries;
-    }
-
-    /**
-     * Returns a flag to indicate whether the interpreter should build indexes (false = no, true = yes)
-     *
-     * @return a flag to indicate whether the interpreter should build indexes (false = no, true = yes)
-     */
-    public boolean shouldBuildIndexes() {
-        return buildIndexes;
-    }
+//    public boolean shouldEvaluateQueries() {
+//        return evaluateQueries;
+//    }
+//
+//    /**
+//     * Returns a flag to indicate whether the interpreter should build indexes (false = no, true = yes)
+//     *
+//     * @return a flag to indicate whether the interpreter should build indexes (false = no, true = yes)
+//     */
+//    public boolean shouldBuildIndexes() {
+//        return buildIndexes;
+//    }
 
     /**
      * Returns true to set the project to read/write human readable files

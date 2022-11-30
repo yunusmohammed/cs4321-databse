2bd5ddce16c54a2d9b88852c393c04d82b44f2d0
Jessica Tweneboah <66502136+jessicatweneboah@users.noreply.github.com>
2022-11-29 22:50:43 -0500

    Refactoring Interpreter Config (#109)

--- src/test/java/com/cs4321/app/InterpreterConfigTest.java
+++ src/test/java/com/cs4321/app/InterpreterConfigTest.java
@@ -17,24 +17,24 @@ class InterpreterConfigTest {
         assertEquals("src/test/resources/input_binary", interpreterConfig1.getInputdir());
         assertEquals("src/test/resources/output", interpreterConfig1.getOutputdir());
         assertEquals("src/test/resources/output", interpreterConfig1.getTempdir());
-        assertFalse(interpreterConfig1.shouldBuildIndexes());
-        assertTrue(interpreterConfig1.shouldEvaluateQueries());
+//        assertFalse(interpreterConfig1.shouldBuildIndexes());
+//        assertTrue(interpreterConfig1.shouldEvaluateQueries());
         assertFalse(interpreterConfig1.isHumanReadable());
 
         InterpreterConfig interpreterConfig2 = new InterpreterConfig(basePath + sep + "config2.txt");
         assertEquals("src/test/resources/input_binary", interpreterConfig2.getInputdir());
         assertEquals("src/test/resources/output", interpreterConfig2.getOutputdir());
         assertEquals("src/test/resources/output", interpreterConfig2.getTempdir());
-        assertTrue(interpreterConfig2.shouldBuildIndexes());
-        assertFalse(interpreterConfig2.shouldEvaluateQueries());
+//        assertTrue(interpreterConfig2.shouldBuildIndexes());
+//        assertFalse(interpreterConfig2.shouldEvaluateQueries());
         assertFalse(interpreterConfig2.isHumanReadable());
 
         InterpreterConfig interpreterConfig3 = new InterpreterConfig(basePath + sep + "config3.txt");
         assertEquals("src/test/resources/input_binary", interpreterConfig3.getInputdir());
         assertEquals("src/test/resources/output", interpreterConfig3.getOutputdir());
         assertEquals("src/test/resources/output", interpreterConfig3.getTempdir());
-        assertTrue(interpreterConfig3.shouldBuildIndexes());
-        assertTrue(interpreterConfig3.shouldEvaluateQueries());
+//        assertTrue(interpreterConfig3.shouldBuildIndexes());
+//        assertTrue(interpreterConfig3.shouldEvaluateQueries());
         assertFalse(interpreterConfig3.isHumanReadable());
     }
 }
